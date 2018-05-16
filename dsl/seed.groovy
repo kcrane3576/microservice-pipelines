d131f302104def394e4aa6c87a59ca937f5ff9be
crane <kcrane@ippon.fr>
2018-05-16 13:58:51 -0400

    setup seed.groovy for part2 of blog

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -1,3 +1,47 @@
-job(jobName) {
-    description("A simple Freestyle Job created from seed.groovy")
+def createPipelineJob(jobName, repoUrl) {
+    pipelineJob(jobName) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repoUrl)
+                        }
+                        branches('master')
+                        extensions {
+                            cleanBeforeCheckout()
+                        }
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
 }
+
+def createMultibranchPipelineJob(jobName, repoUrl) {
+    multibranchPipelineJob(jobName) {
+        branchSources {
+            git {
+                remote(repoUrl)
+                includes('*')
+            }
+        }
+        triggers {
+            cron("H/5 * * * *")
+        }
+    }
+}
+
+def buildPipelineJobs() {
+    def repo = "https://github.com/kcrane3576/"
+    def repoUrl = repo + jobName + ".git"
+    def deployName = jobName + "_deploy"
+    def testName = jobName + "_test"
+
+    createPipelineJob(deployName, repoUrl)
+    createMultibranchPipelineJob(testName, repoUrl)
+}
+
+def pipelineConfig = getPipelineConfig()
+buildPipelineJobs()
